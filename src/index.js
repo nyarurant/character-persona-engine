bd33840d70f6fc7d8f3ea6c4a30546d368b4b46b
ks=hcaptcha <cloud.nyarurant@gmail.com>
2026-08-30 13:47:12 +0900

    feat: export generic runtime state stores

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const { ClaudeCliProvider } = require('./providers/claude-cli');
 const { retrieveVoice, retrieveLore } = require('./retrieval/lexical');
 const { buildSystemPrompt, buildUserPrompt } = require('./context/prompt-builder');
 const { buildPersonaFromCorpus } = require('./build/persona-builder');
+const { MemoryStore } = require('./memory/memory-store');
+const { AffinityStore } = require('./state/affinity-store');
+const { ConversationStateStore } = require('./state/conversation-state');
+const { RuntimeContext } = require('./runtime/runtime-context');
 
 module.exports = {
   loadCharacterPack,
@@ -16,4 +20,8 @@ module.exports = {
   buildSystemPrompt,
   buildUserPrompt,
   buildPersonaFromCorpus,
+  MemoryStore,
+  AffinityStore,
+  ConversationStateStore,
+  RuntimeContext,
 };
